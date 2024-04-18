d86db9abab402d35dafe620e79e5838dcd014ac2
Gustavo Gonzalez <88396719+gustavodeam@users.noreply.github.com>
2024-04-18 18:51:20 -0300

    Limpieza de código
    Se quitaron comentarios y un endpoint que no se usa.

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-/*
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-*/
-//Route::post('/set-person', [PersonController::class, 'store']);
-Route::post('set-person', 'App\Http\Controllers\PersonController@store');
+
 Route::get('person/{email}', 'App\Http\Controllers\PersonController@show');
 Route::post('send-message', 'App\Http\Controllers\MessageController@store');
